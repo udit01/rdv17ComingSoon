91cf35a9189e722a941479af0746bcd82fe5c76c
udit01 <uditjainiitd@gmail.com>
2017-05-21 16:28:33 +0530

    added the 4 responsive images

---
+++ script.js
@@ -0,0 +1,25 @@
+/**
+ * Created by udit01 on 21/5/17.
+ */
+
+
+$(document).ready(function() {
+    $("#fireAnimate").hover(
+        function() {
+            $(this).attr("src", "images/fire-animated.gif");
+        },
+        function() {
+            $(this).attr("src", "images/fire-static.png");
+        }
+    );
+
+    $("#tentAnimate").hover(
+        function() {
+            $(this).attr("src", "images/tent-animated.gif");
+        },
+        function() {
+            $(this).attr("src", "images/tent-static.svg");
+        }
+    );
+
+});
