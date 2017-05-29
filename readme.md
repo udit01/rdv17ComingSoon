b7c11f5d58e93b834772a8b926694f81e992e86b
udit01 <uditjainiitd@gmail.com>
2017-05-30 05:22:05 +0530

    added video unlooped with Ref-image overlay

--- readme.md
+++ readme.md
@@ -1,1 +1,2 @@
 This is the website which will remain at the domain name until the fully functional website is made.
+
--- script.js
+++ script.js
@@ -3,23 +3,39 @@
  */
 
 
-$(document).ready(function() {
-    // $("#fireAnimate").hover(
-    //     function() {
-    //         $(this).attr("src", "images/fire-animated.gif");
-    //     },
-    //     function() {
-    //         $(this).attr("src", "images/fire-static.png");
-    //     }
-    // );
-    //
-    // $("#tentAnimate").hover(
-    //     function() {
-    //         $(this).attr("src", "images/tent-animated.gif");
-    //     },
-    //     function() {
-    //         $(this).attr("src", "images/tent-static.svg");
-    //     }
-    // );
+var vid = document.getElementById("bgvid");
 
+function vidFade() {
+    vid.classList.add("stopfade");
+}
+
+vid.addEventListener('ended', function()
+{
+// only functional if "loop" is removed
+    vid.pause();
+// to capture IE10
+    vidFade();
 });
+
+
+
+// $(document).ready(function() {
+//     $("#fireAnimate").hover(
+//         function() {
+//             $(this).attr("src", "images/fire-animated.gif");
+//         },
+//         function() {
+//             $(this).attr("src", "images/fire-static.png");
+//         }
+//     );
+//
+//     $("#tentAnimate").hover(
+//         function() {
+//             $(this).attr("src", "images/tent-animated.gif");
+//         },
+//         function() {
+//             $(this).attr("src", "images/tent-static.svg");
+//         }
+//     );
+//
+// });
